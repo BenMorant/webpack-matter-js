ae50998edfa875a1a1d1084735d248e1895c405e
Ben Mor </>
2021-04-27 16:30:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# a dummy JS frontend
+
+based upon [Robin Wieruch's tutorials](https://www.robinwieruch.de/) , \
+with :
+
+- webpack
+- babel
+- eslint
+- prettier
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:8080](http://localhost:8080) to view it in the browser.
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin({
+      fix: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+  },
+}
